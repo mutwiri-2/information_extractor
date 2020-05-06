113e890d8a4a29b425b17472502debb0c937454d
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-06 15:43:18 +0300

    define kenyan number regex

---
+++ kenyan_numbers.py
@@ -0,0 +1,13 @@
+#! python3
+
+# kenyan_numbers.py - a program to find kenyan numbers from text copied to the clipboard, identify its mobile network operator and then copy the results back to the clipboard
+
+# TODO - define kenyan number regex
+import pyperclip, re
+
+kenyan_number_regex = re.compile(r"""(
+    (+254|0)   # country code
+    (\s|-|,)?   # separator
+    (7\d\d)     # prefix
+    (\d{6})     # remaining numbers
+)""", re.VERBOSE)
