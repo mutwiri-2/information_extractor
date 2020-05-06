f505287a0352f57a3c4d2010ace74419f5e7f8af
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-06 10:46:11 +0300

    add scope of program

---
+++ phone_number_and_email.py
@@ -0,0 +1,15 @@
+#! python3
+
+# phone_number_and_email.py - a program to find phone numbers (USA format) and emails from text copied to the clipboard and paste back the results to the clipboard.
+
+# TODO - write a phone number regex
+
+# TODO - write an email regex
+
+# TODO - get text from clipboard and find matches
+
+# TODO - write a phone number regex
+
+# TODO - neatly format the output and copy it to the clipboard ready for use
+
+
