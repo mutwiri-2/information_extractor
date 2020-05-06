629ca16b4c3c3140d0476b021044f2c641085ec0
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-06 23:04:44 +0300

    shell script runs python script

--- email_extractor/email_extractor.sh
+++ email_extractor/email_extractor.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python3 -O /home/mutwiri2/Desktop/information_extractor/email_extractor/email_extractor.sh
+python3 -O /home/mutwiri2/Desktop/information_extractor/email_extractor/email_extractor.py
 bash
