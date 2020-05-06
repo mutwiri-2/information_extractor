0ab99b1ea826f54333bfcff4bb02b09f214c0ff0
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-06 11:56:46 +0300

    define phone number regex

--- phone_number_and_email.py
+++ phone_number_and_email.py
@@ -2,7 +2,17 @@
 
 # phone_number_and_email.py - a program to find phone numbers (USA format) and emails from text copied to the clipboard and paste back the results to the clipboard.
 
-# TODO - write a phone number regex
+import pyperclip, re 
+# phone number regex
+phone_number = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?   # area code - optional
+    (\s|-|\.)? # separator - optional depending on area code existence
+    (\d{3})  # first three digits
+    (\s|-|\.) # separator
+    (\d{4})  # next four digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension - optional
+)''', re.VERBOSE)
+
 
 # TODO - write an email regex
 
