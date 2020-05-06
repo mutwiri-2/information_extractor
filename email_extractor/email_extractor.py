391e8db6757f98c1cff879145305726318828644
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-06 20:05:46 +0300

    email extractor

---
+++ email_extractor/email_extractor.py
@@ -0,0 +1,34 @@
+#! python3
+
+# email_extractor.py - a program to find emails from text copied to the clipboard and paste back matches found to the clipboard.
+
+import pyperclip, re 
+
+# email regex
+email_regex = re.compile(r"""(
+    ([a-zA-Z0-9_%.+-])+  #username
+    @  # the at symbol
+    ([a-zA-Z0-9-.])  # domain-name
+    (\.[a-zA-Z]{2,4})  # top-level domain - dot something .com, .io, .org etc
+)""", re.VERBOSE)
+
+
+# get text from clipboard and 
+text = str(pyperclip.paste())
+
+# find matches
+matches = []  # list to store strings that match our regexes
+
+# find email matches
+for groups in email_regex.findall(text):
+    matches.append(groups[0])
+
+# neatly format the output and copy it to the clipboard ready for use
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print("Copied to clipboard")
+    print('\n'.join(matches))
+else:
+    print("No emails found")
+
+
