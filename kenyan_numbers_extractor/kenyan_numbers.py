2c91757e763ee33f319a093b0ebf24b279fe038c
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-06 21:34:15 +0300

    get group 0 of matched

--- kenyan_numbers_extractor/kenyan_numbers.py
+++ kenyan_numbers_extractor/kenyan_numbers.py
@@ -20,9 +20,7 @@ data = str(pyperclip.paste())
 numbers = []
 
 for groups in kenyan_number_regex.findall(data):
-    phone_num = ' '.join([groups[1], groups[3], groups[5]])
-    # phone_num = ' '.join(['+254', groups[3], groups[5]])
-numbers.append(phone_num)
+    numbers.append(groups[0])
 
 # neatly format and output the numbers found
 if len(numbers) > 0:
