592a002576b525afaa7746c02e142ae5dd9e5fb5
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-07 05:15:37 +0300

    add a half-second delay before copied to clipboard message

--- kenyan_numbers_extractor/kenyan_numbers_extractor.py
+++ kenyan_numbers_extractor/kenyan_numbers_extractor.py
@@ -3,7 +3,7 @@
 # kenyan_numbers.py - a program to find kenyan numbers from text copied to the clipboard, identify its mobile network operator and then copy the results back to the clipboard
 
 # define kenyan number regex
-import pyperclip, re
+import pyperclip, re, time
 
 kenyan_number_regex = re.compile(r"""(
     (\+254|0)   # country code
@@ -30,6 +30,7 @@ count = len(numbers)
 if count > 0:
     pyperclip.copy('\n'.join(numbers))
     print('\n'.join(numbers))
+    time.sleep(0.5)
     print(f"{count} numbers copied to clipboard")
 else:
     print("No Kenyan phone numbers found.")
